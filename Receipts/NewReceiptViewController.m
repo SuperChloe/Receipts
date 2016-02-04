f5b5baf497e85a3efcfab211e9cc8c9a875ffdb7
Chloe Horgan <super.chloe@gmail.com>
2016-02-04 13:28:56 -0500

    Storyboard and autolayout

---
+++ Receipts/NewReceiptViewController.m
@@ -0,0 +1,35 @@
+//
+//  NewReceiptViewController.m
+//  Receipts
+//
+//  Created by Chloe on 2016-02-04.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import "NewReceiptViewController.h"
+
+@interface NewReceiptViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *amountField;
+@property (weak, nonatomic) IBOutlet UITextView *descrField;
+@property (weak, nonatomic) IBOutlet UITableView *categoryTable;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UIButton *doneButton;
+
+@end
+
+@implementation NewReceiptViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)donePressed:(id)sender {
+}
+
+@end
--- Receipts/ViewController.m
+++ Receipts/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIButton *addReciptButton;
 
 @end
 
@@ -16,12 +19,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)addRecipt:(id)sender {
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    return cell;
 }
 
 @end
